bcb6588c93f8efea27408676e206bcded58cf3ab
Ben <56596113+69BENGUNN420@users.noreply.github.com>
2020-02-25 20:28:22 +0200

    Add files via upload

---
+++ movePlayerToThePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayerToThePoint : MonoBehaviour
+{
+	GameObject go;
+	Vector3 heathen;
+	Transform moveThis;
+	float speed;
+	playerMovement PlayerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+		go = GameObject.Find ("Player");
+		PlayerMovement = go.GetComponent <playerMovement> ();
+		speed = PlayerMovement.speed;
+		heathen.Set(0, 2, 0);
+		moveThis = PlayerMovement.moveThis;
+	}
+	
+    // Update is called once per frame
+    void Update()
+    {
+		heathen = PlayerMovement.heathen;
+		float step = speed * Time.deltaTime;
+		if (moveThis.position != heathen){
+			moveThis.transform.position = Vector3.MoveTowards(moveThis.position, new Vector3(heathen.x, moveThis.position.y, heathen.z), step);
+		}
+    }
+}
---
+++ playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{	
+	public Vector3 heathen;
+	
+	public Transform moveThis;
+	public LayerMask hitLayers;
+	public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heathen.Set(0, 2, 0);
+    }
+	
+    
+	
+	
+	// Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0)){
+			Vector3 mouse = Input.mousePosition;
+			Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+			RaycastHit hit;
+			
+			if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+			{
+				heathen=hit.point;
+			}
+		}
+	}
+		
+}
